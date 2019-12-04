280f44c1f4405de74eab17f7dace7814d6d2227b
kimyungi <seebud@naver.com>
2019-12-04 15:47:01 +0900

    z

--- spring_ns/src/main/java/app/spring/sw/controller/Mem_InsertController.java
+++ spring_ns/src/main/java/app/spring/sw/controller/Mem_InsertController.java
@@ -12,20 +12,9 @@ import app.spring.sw.vo.MemVo;
 public class Mem_InsertController {
 	@Autowired private MemService service;
 	
-	@RequestMapping(value="/insert",method=RequestMethod.GET)
-	public String insertFrom(){
-		return  ".sw.ss";
-	}
-	
 	@RequestMapping(value="/insert",method=RequestMethod.POST)
 	public String insert(MemVo vo){
-		try {
-			//service를 통해 vo객체의 insert메소드를 실행한다.
-			service.insert(vo);
-			return "success";
-		} catch (Exception e) {
-			e.printStackTrace();
-			return "fail";
-		}
+		System.out.println("test");
+		return ".main";
 	}
 }
