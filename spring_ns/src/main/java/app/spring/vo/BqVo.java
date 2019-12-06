0db56ca1e46121a9818e52bdc1db72d1ec5fac0d
박진성 <56255692+jpeter3459@users.noreply.github.com>
2019-12-06 09:51:13 +0900

    MemVo m_status 추가 BqVo 추가

---
+++ spring_ns/src/main/java/app/spring/vo/BqVo.java
@@ -0,0 +1,75 @@
+package app.spring.vo;
+
+import java.sql.Date;
+
+public class BqVo {
+	private int bq_num;
+	private int d_num;
+	private String bq_content;
+	private Date bq_date;
+	private String bq_reply;
+	private String bq_state;
+	
+	public BqVo(){}
+
+	public BqVo(int bq_num, int d_num, String bq_content, Date bq_date, String bq_reply, String bq_state) {
+		super();
+		this.bq_num = bq_num;
+		this.d_num = d_num;
+		this.bq_content = bq_content;
+		this.bq_date = bq_date;
+		this.bq_reply = bq_reply;
+		this.bq_state = bq_state;
+	}
+
+	public int getBq_num() {
+		return bq_num;
+	}
+
+	public void setBq_num(int bq_num) {
+		this.bq_num = bq_num;
+	}
+
+	public int getD_num() {
+		return d_num;
+	}
+
+	public void setD_num(int d_num) {
+		this.d_num = d_num;
+	}
+
+	public String getBq_content() {
+		return bq_content;
+	}
+
+	public void setBq_content(String bq_content) {
+		this.bq_content = bq_content;
+	}
+
+	public Date getBq_date() {
+		return bq_date;
+	}
+
+	public void setBq_date(Date bq_date) {
+		this.bq_date = bq_date;
+	}
+
+	public String getBq_reply() {
+		return bq_reply;
+	}
+
+	public void setBq_reply(String bq_reply) {
+		this.bq_reply = bq_reply;
+	}
+
+	public String getBq_state() {
+		return bq_state;
+	}
+
+	public void setBq_state(String bq_state) {
+		this.bq_state = bq_state;
+	}
+	
+	
+}
+	
