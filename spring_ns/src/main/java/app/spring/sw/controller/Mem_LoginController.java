bb039ad30dd7a203c202966a49d5faf819080a35
SW-Jeon <w2263@daum.net>
2019-12-09 19:56:52 +0900

    회원전화번호찾기 완료

--- spring_ns/src/main/java/app/spring/sw/controller/Mem_LoginController.java
+++ spring_ns/src/main/java/app/spring/sw/controller/Mem_LoginController.java
@@ -1,12 +1,15 @@
 package app.spring.sw.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import app.spring.sw.service.MemService;
 import app.spring.vo.MemVo;
@@ -44,19 +47,17 @@ public class Mem_LoginController {
         }
         
         @RequestMapping(value="/swMem/findPhone",method=RequestMethod.POST)
-        public String findPhone(String m_phone,String m_name,String m_mail,HttpSession session,Model model,MemVo vo){
+        public String findPhone(String m_name,String m_mail,HttpServletRequest req, Model model,MemVo vo){
         	String phone=service.getPhone(vo);
-        	Object name=session.getAttribute(m_name);
-        	Object mail=session.getAttribute(m_mail);
-        	//String name=vo.getM_name();
-        	//String mail=vo.getM_mail();
+        	String name=req.getParameter("m_name");
+        	String mail=req.getParameter("m_mail");
         	System.out.println(mail);
         	
-        	if (name.equals(m_name) && mail.equals(m_mail)){
-        		session.setAttribute("phone", phone);
-        		model.addAttribute("code", "find");
-        	}else  {
+        	 if(phone==null) {
         		model.addAttribute("code", "fail");
+        	}else if(name.equals(m_name) && mail.equals(m_mail)){
+        		model.addAttribute("phone", phone);
+        		model.addAttribute("code", "find");
         	}
         	return ".swMem.findPhoneOk";
         }
