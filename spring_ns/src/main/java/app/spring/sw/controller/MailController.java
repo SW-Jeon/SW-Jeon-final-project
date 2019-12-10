3208c25abd408f34f029022a7c85306bf037a4b8
SW-Jeon <w2263@daum.net>
2019-12-10 20:37:09 +0900

    메일

--- spring_ns/src/main/java/app/spring/sw/controller/MailController.java
+++ spring_ns/src/main/java/app/spring/sw/controller/MailController.java
@@ -9,28 +9,34 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import app.spring.sw.service.MemService;
+import app.spring.vo.MemVo;
 
 @Controller
 public class MailController {
 	@Autowired
 	  private JavaMailSender mailSender;
-	 
-	 
+	  private MemService service;
+
 	  // mailForm
-	  @RequestMapping(value = "/swMem/find")
+	  @RequestMapping(value = "/swMem/find",method=RequestMethod.GET)
 	  public String mailForm() {
-	   
 	    return ".swMem.mailSending";
 	  }  
 	 
 	  // mailSending 코드
-	  @RequestMapping(value = "/swMem/findPwd")
-	  public String mailSending(HttpServletRequest request,Model model) {
-	   
+	  @RequestMapping(value = "/swMem/findPwd",method=RequestMethod.POST)
+	  public String mailSending(HttpServletRequest request,Model model,MemVo vo) {
+		  String phone=request.getParameter("m_phone");
+		  String name=request.getParameter("m_name");
+		  String mail=request.getParameter("m_mail");
+		 service.changePwd(vo);
+		  
 	    String setfrom = "w2263@daum.net";         //보내는 이
 	    String tomail  ="w2263@daum.net";	//받는 이
-	    
-	    String title   = "회원님의 임시비밀번호를 전송합니다.";      // 제목
+	    String title   = "회원님의 새로운 임시비밀번호를 전송합니다.";      // 제목
 	    String content = request.getParameter("임시비밀번호"+new RandomString().create(8)+ "입니다.");    // 내용
 	   
 	    try {
@@ -40,7 +46,8 @@ public class MailController {
 	      messageHelper.setTo(tomail);     // 받는사람 이메일
 	      messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
 	      messageHelper.setText(content);  // 메일 내용
-	     
+	      if()
+	      
 	      mailSender.send(message);
 	    } catch(Exception e){
 	      System.out.println(e);
